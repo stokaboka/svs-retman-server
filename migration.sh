66a808fb1e4b60d1febcce4e892104ceab7293b4
softex-horev <i.horev@softex-group.com>
2019-03-29 16:47:50 +0300

    add migration.sh

---
+++ migration.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+./node_modules/typeorm/cli.js query "DROP TABLE svsbeginners.Dictionary, svsbeginners.Groups, svsbeginners.LessonStages, svsbeginners.migrations, svsbeginners.Phases, svsbeginners.Steps, svsbeginners.Users, svsbeginners.UsersGroups, svsbeginners.Cue, svsbeginners.UsersResults;"
+
+./node_modules/typeorm/cli.js migration:run
+
+exit 0
